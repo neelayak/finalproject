8a5f3e6a87ca6791824a5a2fc2ca0b9356ef3b4b
neelayak <yvvas10@gmail.com>
2024-04-13 03:07:34 +0530

    new field

--- lib/app/detailsScreen.dart
+++ lib/app/detailsScreen.dart
@@ -18,7 +18,9 @@ class _DetailsScreenState extends State<DetailsScreen> {
   String? temperature;
   String? oxygenLevel;
   String? _selectedItem;
+  String? patient_id;
   String? filename;
+  String? patient_age;
 
   @override
   Widget build(BuildContext context) {
@@ -26,188 +28,244 @@ class _DetailsScreenState extends State<DetailsScreen> {
       appBar: AppBar(
         title: Text('HealthSync Emergency'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  'Enter Blood Pressure ',
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
-                ),
-              ),
-              TextFormField(
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                decoration: InputDecoration(
-                  labelText: 'Blood Pressure',
-                  hintText: 'Blood Pressure',
-                  prefixIcon: Icon(Icons.person),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Enter patient identifier number ',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
                   ),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter blood pressure';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  bloodPressure = value;
-                },
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  'Enter Temperature',
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                TextFormField(
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    labelText: 'Patient id',
+                    hintText: 'Patient id',
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter blood pressure';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    patient_id = value;
+                  },
                 ),
-              ),
-              TextFormField(
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                decoration: InputDecoration(
-                  labelText: 'Temperature',
-                  hintText: 'Temperature',
-                  prefixIcon: Icon(Icons.person),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Enter patient Age',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
                   ),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter temperature';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  temperature = value;
-                },
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: Text(
-                  'Enter Oxygen Level ',
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                TextFormField(
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    labelText: 'Patient Age',
+                    hintText: 'Patient Age',
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter blood pressure';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    patient_id = value;
+                  },
                 ),
-              ),
-              TextFormField(
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                decoration: InputDecoration(
-                  labelText: 'Oxygen Level',
-                  hintText: 'Oxygen Level',
-                  prefixIcon: Icon(Icons.person),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Enter Blood Pressure ',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
                   ),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter oxygen level';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  oxygenLevel = value;
-                },
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      child: Text(
-                        'Select Hospital',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 18),
-                      ),
+                TextFormField(
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    labelText: 'Blood Pressure',
+                    hintText: 'Blood Pressure',
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    SizedBox(width: 20),
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: 12.0),
-                      child: DropdownButton<String>(
-                        value: _selectedItem,
-                        items: <String>[
-                          'Hospital 1',
-                          'Hospital 2',
-                          'Hospital 3'
-                        ].map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (newValue) {
-                          setState(() {
-                            _selectedItem = newValue;
-                          });
-                        },
-                        hint: Text('Select an option'),
-                        underline: SizedBox(), // removes the underline border
-                      ),
-                    ),
-                  ],
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter blood pressure';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    bloodPressure = value;
+                  },
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      child: Text(
-                        'Select EKG file',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 18),
-                      ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Enter Temperature',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    labelText: 'Temperature',
+                    hintText: 'Temperature',
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    SizedBox(width: 20),
-                    Container(
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter temperature';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    temperature = value;
+                  },
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    'Enter Oxygen Level ',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    labelText: 'Oxygen Level',
+                    hintText: 'Oxygen Level',
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter oxygen level';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    oxygenLevel = value;
+                  },
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          'Select Hospital',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 18),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
                         padding: EdgeInsets.symmetric(horizontal: 12.0),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            FilePickerResult? result =
-                                await FilePicker.platform.pickFiles(
-                              type: FileType.custom,
-                              allowedExtensions: ['pdf'],
+                        child: DropdownButton<String>(
+                          value: _selectedItem,
+                          items: <String>[
+                            'Hospital 1',
+                            'Hospital 2',
+                            'Hospital 3'
+                          ].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
                             );
-                            if (result != null) {
-                              File file = File(result.files.single.path!);
-                              setState(() {
-                                filename = basename(file.path);
-                              });
-                            } else {
-                              // User canceled the picker
-                            }
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              _selectedItem = newValue;
+                            });
                           },
-                          child: Text(
-                              (filename != null) ? filename! : 'select file'),
-                        )),
-                  ],
+                          hint: Text('Select an option'),
+                          underline: SizedBox(), // removes the underline border
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    // Here you can do whatever you want with the collected data
-                    print('Blood Pressure: $bloodPressure');
-                    print('Temperature: $temperature');
-                    print('Oxygen Level: $oxygenLevel');
-                  }
-                },
-                child: Text('Submit'),
-              ),
-            ],
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          'Select EKG file',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 18),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Container(
+                          padding: EdgeInsets.symmetric(horizontal: 12.0),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              FilePickerResult? result =
+                                  await FilePicker.platform.pickFiles(
+                                type: FileType.custom,
+                                allowedExtensions: ['pdf'],
+                              );
+                              if (result != null) {
+                                File file = File(result.files.single.path!);
+                                setState(() {
+                                  filename = basename(file.path);
+                                });
+                              } else {
+                                // User canceled the picker
+                              }
+                            },
+                            child: Text(
+                                (filename != null) ? filename! : 'select file'),
+                          )),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      // Here you can do whatever you want with the collected data
+                      print('Blood Pressure: $bloodPressure');
+                      print('Temperature: $temperature');
+                      print('Oxygen Level: $oxygenLevel');
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
